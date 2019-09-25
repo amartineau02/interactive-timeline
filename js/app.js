8cbcefb94444a91a0b5848a003049d3efc694f1c
Amber M <amartineau02@wildcats.jwu.edu>
2019-09-25 15:18:03 -0400

    tips and tricks 1

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
-$(document).foundation()
+$(document).foundation();
+
+$('.panel').not('#rehobothvillage').hide();
+$('.intro').hide();
+
+
+$('.mainmenu').on('click','a',function(e){
+  e.preventDefault();
+  var curr = $(this).attr('href');
+  $('.panel').hide();
+  $(curr).show();
+});
+
+
+
+$('.auxmenu').on('click','a',function(e){
+  e.preventDefault();
+  $('.intro').toggle();
+});
